33294924634325c0c5dae1530b5b56b187e8543a
le-fromage <le_fromage@outlook.fr>
2014-11-02 13:44:39 +0100

    Better var name

--- BrainFuck.lua
+++ BrainFuck.lua
@@ -4,6 +4,14 @@ Callback.Bind('Load', function()
   Callback.Bind('RecvChat', function(from, msg) OnChatMessage(from, msg) end)
 end)
 
+local memory 	= {0, 0, 0, 0}
+local stack		= {}
+local mi		= 1  -- memory iterator
+local sc		= 0  -- stack counter
+local pi		= 1  -- program iterator
+local buffer	= "" -- input buffer
+local bi		= -1  -- buffer iterator
+
 function OnStart()
 	menu = MenuConfig("Brainfuck")
 
@@ -13,19 +21,54 @@ function OnStart()
 end
 
 function OnChatMessage(from, msg)
-	if (menu.Enable:Value() or string.starts(msg, "/bf")) then
+	if menu.Enable:Value() then
 		print(msg)
 		Run(msg)
-		if (menu.Local:Value()) then
+		if menu.Local:Value() then
 			Game.Chat.Block()
 		end
 	end
 end
 
-function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
-end
+function Run(prog)
+	while pi <= #prog do
+		local c = prog:sub(pi, pi)
 
+		if c == '.' then
+			Game.Chat.Send(string.char(memory[mi]))
+		elseif c == ',' then
+			memory[mi] = io.read(1)
+		elseif c == '>' then
+			mi = mi + 1
+			if (memory[mi] == nil) then
+				memory[mi] = 0
+			end
+		elseif c == '<' then
+			mi = mi - 1
+		elseif c == '+' then
+			memory[mi] = memory[mi] + 1
+		elseif c == '-' then
+			memory[mi] = memory[mi] - 1
+		elseif c == '[' then
+			if memory[mi] == 0 then
+				pi = SkipBlock(c) - 1
+				if pi <= -42 then
+					return -- Error
+				end
+			else
+				sc = sc + 1
+				stack[sc] = pi
+			end
+		elseif c == ']' then
+			if sc <= 0 then
+				return -- Error
+			end
+			pi = stack[sc] - 1
+			sc = sc - 1
+		end
+		pi = pi + 1
+	end
+end
 
 function SkipBlock(i)
 	local tmp = 0
@@ -45,49 +88,6 @@ function SkipBlock(i)
 	return -42
 end
 
-function Run(prog)
-	local i = 1
-
-	while i <= #prog do
-		local c = prog:sub(i, i)
-
-		if c == '.' then
-			Game.Chat.Send(string.char(memory[pc]))
-		elseif c == ',' then
-			memory[pc] = io.read(1)
-		elseif c == '>' then
-			pc = pc + 1
-			if (memory[pc] == nil) then
-				memory[pc] = 0
-			end
-		elseif c == '<' then
-			pc = pc - 1
-		elseif c == '+' then
-			memory[pc] = memory[pc] + 1
-		elseif c == '-' then
-			memory[pc] = memory[pc] - 1
-		elseif c == '[' then
-			if memory[pc] == 0 then
-				i = SkipBlock(i) - 1
-				if i == -42 then
-					return "PATATE" -- Error
-				end
-			else
-				sc = sc + 1
-				stack[sc] = i
-			end
-		elseif c == ']' then
-			if sc <= 0 then
-				return -- Error
-			end
-			i = stack[sc] - 1
-			sc = sc - 1
-		end
-		i = i + 1
-	end
+function string.starts(String, Start)
+   return string.sub(String, 1, string.len(Start)) == Start
 end
-
-memory 	= {0, 0, 0, 0}
-stack	= {}
-pc		= 1 -- program counter
-sc		= 0 -- stack counter
