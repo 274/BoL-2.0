882cb51e225ad20a8b6c3d2bf28035f389eefa2a
le-fromage <le_fromage@outlook.fr>
2014-11-03 21:59:43 +0100

    Brainfuck : bugfix + add infinite loop protection

--- BrainFuck.lua
+++ BrainFuck.lua
@@ -10,34 +10,64 @@ local mi		= 1  -- memory iterator
 local sc		= 0  -- stack counter
 local pi		= 1  -- program iterator
 local buffer	= "" -- input buffer
-local bi		= -1  -- buffer iterator
+local _r		= false -- request read
+local prog		= "" -- program
+local Watchdog	= 0
 
 function OnStart()
 	menu = MenuConfig("Brainfuck")
 
+	menu:Icon('fa-stack-overflow')
 	menu:Section('Brainfuck', 'Brainfuck')
 	menu:Boolean('Enable', 'Enable', true)
 	menu:Boolean('Local', 'Output to local chat', true)
+	menu:Slider('Watchdog', 'Infinite loop protection', 1000, 100, 314, 4242)
 end
 
 function OnChatMessage(from, msg)
 	if menu.Enable:Value() then
-		print(msg)
-		Run(msg)
+		Game.Chat.Block()
 		if menu.Local:Value() then
-			Game.Chat.Block()
+			Game.Chat.Print(msg)
+		else
+			Game.Chat.Send(msg)
 		end
+		if _r then
+			buffer = msg
+			_r = false
+			Run()
+		else
+			print(msg)
+			prog = msg
+			Run()
+		end
+
 	end
 end
 
-function Run(prog)
+function Run()
 	while pi <= #prog do
 		local c = prog:sub(pi, pi)
 
+		if watchdog >= menu.Watchdog:Value() then
+			reinit()
+			return
+		end 
+
 		if c == '.' then
-			Game.Chat.Send(string.char(memory[mi]))
+			if menu.Local:Value() then
+				Game.Chat.Print(string.char(memory[mi]))
+			else
+				Game.Chat.Send(string.char(memory[mi]))
+			end
 		elseif c == ',' then
-			memory[mi] = io.read(1)
+			if #buffer > 0 then
+				memory[mi] = string.byte(buffer)
+				string.sub(buffer, 1)
+			else
+				_r = true
+				return
+			end
 		elseif c == '>' then
 			mi = mi + 1
 			if (memory[mi] == nil) then
@@ -67,7 +97,9 @@ function Run(prog)
 			sc = sc - 1
 		end
 		pi = pi + 1
+		watchdog = watchdog + 1
 	end
+	reinit()
 end
 
 function SkipBlock(i)
@@ -88,6 +120,14 @@ function SkipBlock(i)
 	return -42
 end
 
-function string.starts(String, Start)
-   return string.sub(String, 1, string.len(Start)) == Start
+function reinit() -- set all var to default
+	memory 	= {0, 0, 0, 0}
+	stack	= {}
+	mi		= 1
+	sc		= 0
+	pi		= 1
+	buffer	= ""
+	_r		= false
+	prog	= ""
+	Watchdog	= 0
 end
